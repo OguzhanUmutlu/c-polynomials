042515b90459974d657fc37737a65233b5b1f659
Oğuzhan <oguzhan.umutlu123@gmail.com>
2023-11-12 13:37:48 +0300

    added solvePolynomial(Polynomial polynomial, uint iterations, uint x0)

--- main.c
+++ main.c
@@ -321,6 +321,17 @@ Polynomial approximatePolynomialAZero(MathFn fn, uint iterations) {
     return polynomial;
 }
 
+num_t solvePolynomial(Polynomial polynomial, uint iterations, num_t x0) {
+    // finds the real root which is closest to given "x0" value
+    num_t x = x0;
+    Polynomial derivative = derivePolynomial(polynomial);
+    for (uint i = 0; i < iterations; i++) {
+        x = x - evaluatePolynomial(polynomial, x) / evaluatePolynomial(derivative, x);
+    }
+    free(derivative.terms);
+    return x;
+}
+
 void printAsPower(uint number) {
     char numberString[20];
     sprintf(numberString, "%d", number);
@@ -404,6 +415,15 @@ int main() {
     // 210.000000x⁶ + 252.000000x⁵ +
     // 210.000000x⁴ + 120.000000x³ +
     // 45.000000x² + 10.000000x + 1.000000
+
+    Polynomial solving = CreatePolynomial(2, 1, 1); // x + 1
+    solving = multiplyTwoPolynomials(solving, CreatePolynomial(2, 2, 1)); // x + 2
+    solving = multiplyTwoPolynomials(solving, CreatePolynomial(2, 3, 1)); // x + 3
+    solving = multiplyTwoPolynomials(solving, CreatePolynomial(2, 4, 1)); // x + 4
+    solving = multiplyTwoPolynomials(solving, CreatePolynomial(2, 5, 1)); // x + 5
+    solving = multiplyTwoPolynomials(solving, CreatePolynomial(2, 6, 1)); // x + 6
+
+    printf("The solution to the polynomial: %Lf", solvePolynomial(solving, 20, 5));
     return 0;
 }
 
